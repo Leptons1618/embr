223561f7c3736f06007117dc08ff361b286034ae
Anish Giri <anishgiri163@gmail.com>
2026-09-24 18:16:31 +0530

    fix(charts): publish testable module artifact

--- modules/charts/web/src/components/apexcharts/react/ApexCharts.tsx
+++ modules/charts/web/src/components/apexcharts/react/ApexCharts.tsx
@@ -15,7 +15,7 @@ import type { ForwardedRef } from 'react'
 import { extend, merge } from 'lodash'
 
 export type ApexChartProps = {
-  type: ApexChart['type']
+  type: NonNullable<NonNullable<ApexOptions['chart']>['type']>
   options: ApexOptions
   series: ApexOptions['series']
   width: number | string
